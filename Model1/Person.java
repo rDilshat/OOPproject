ec57e829eb326307cc2c24fdd6815388e45916d5
Dilshat <rakhmatulindilshat@gmail.com>
2024-11-17 17:40:54 +0500

    Initial commit: Добавлен пример диаграммы

---
+++ Model1/Person.java
@@ -0,0 +1,58 @@
+
+import java.io.*;
+import java.util.*;
+
+/**
+ * 
+ */
+public class Person {
+
+    /**
+     * Default constructor
+     */
+    public Person() {
+    }
+
+    /**
+     * 
+     */
+    private String name;
+
+    /**
+     * 
+     */
+    private int age;
+
+    /**
+     * @param name 
+     * @param age
+     */
+    public Person(String name, int age) {
+        // TODO implement here
+    }
+
+    /**
+     * @return
+     */
+    public String toString() {
+        // TODO implement here
+        return "";
+    }
+
+    /**
+     * @return
+     */
+    public String getName() {
+        // TODO implement here
+        return "";
+    }
+
+    /**
+     * @return
+     */
+    public int getAge() {
+        // TODO implement here
+        return 0;
+    }
+
+}
---
+++ Model1/Student.java
@@ -0,0 +1,68 @@
+
+import java.io.*;
+import java.util.*;
+
+/**
+ * 
+ */
+public class Student extends Person {
+
+    /**
+     * Default constructor
+     */
+    public Student() {
+    }
+
+    /**
+     * 
+     */
+    private String major;
+
+    /**
+     * 
+     */
+    private int year;
+
+    /**
+     * 
+     */
+    private String program;
+
+    /**
+     * Accepted values for Student
+     * @param name 
+     * @param age 
+     * @param major 
+     * @param year 
+     * @param program
+     */
+    public Student(String name, int age, String major, int year, String program) {
+        // TODO implement here
+    }
+
+    /**
+     * Returns major of student
+     * @return
+     */
+    public String getMajor() {
+        // TODO implement here
+        return "";
+    }
+
+    /**
+     * @return
+     */
+    public int getYear() {
+        // TODO implement here
+        return 0;
+    }
+
+    /**
+     * @return
+     */
+    public String getProgram() {
+        // TODO implement here
+        return "";
+    }
+
+}
